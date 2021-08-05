113388fa83a7f15bdb4103cf3ab67ec1327f95af
gitsinaukoding <footprintstshirt@gmail.com>
2021-08-05 16:48:45 +0700

    crud murni php

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # php_crud_mysqli
+
+- bikin database
+- bikin tabel "siswa" jumlah kolom 9: nis, nama, kelas, alamat, no hp, ttl, jenis kelamin, alamat
+    - nis: varchar 20 primary
+    - nama: varchar 50
+    - no_hp: varchar 15
+    - tempat_lahir: varchar 20
+    - tgl_lahir: date
+    - jenis_kelamin: enum L, P
+    - alamat: varchar 100
+- koneksi.php
+- create.php
+- index.php
+- update.php
+- delete.php
+
+- membuat view html
+
+- menggabungkan view dengan php
--- create.php
+++ create.php
@@ -1,38 +1,26 @@
 <?php
-//mulai proses tambah data
 
+//inlcude atau memasukkan file koneksi ke database
+include('koneksi.php');
 
-//cek dahulu, jika tombol tambah di klik
-if(isset($_POST['tambah'])){
+$nis = 'A11.123456.79';	
+$nama = 'Andre';	
+$no_hp = '085123456789';	
+$tmpt_lahir = 'Jakarta';	
+$tgl_lahir = '1998-08-17';	
+$jenis_kelamin = 'L';	
+$alamat = 'Jalan Maju No 000 RT 0 RW 0 Kelurahan Maju Kecamatan Maju Kota Maju';	
 
-	//inlcude atau memasukkan file koneksi ke database
-	include('koneksi.php');
+$sql = "INSERT INTO siswa (nis, nama, no_hp, tmpt_lahir, tgl_lahir, jenis_kelamin, alamat) 
+		VALUES('$nis', '$nama', '$no_hp', '$tmpt_lahir', '$tgl_lahir', '$jenis_kelamin', '$alamat')";
 
-	//jika tombol tambah benar di klik maka lanjut prosesnya
-	$nis		= $_POST['nis'];	//membuat variabel $nis dan datanya dari inputan NIS
-	$nama		= $_POST['nama'];	//membuat variabel $nama dan datanya dari inputan Nama Lengkap
-	$kelas		= $_POST['kelas'];	//membuat variabel $kelas dan datanya dari inputan dropdown Kelas
-	$jurusan	= $_POST['jurusan'];	//membuat variabel $jurusan dan datanya dari inputan dropdown Jurusan
-
-	//melakukan query dengan perintah INSERT INTO untuk memasukkan data ke database
-	$sql = "INSERT INTO siswa VALUES(NULL, '$nis', '$nama', '$kelas', '$jurusan')";
-
-	//jika query input sukses
-	if (mysqli_query($koneksi, $sql)) {
-		echo 'Data berhasil di tambahkan! ';		//Pesan jika proses tambah sukses
-		echo '<a href="index.php">Kembali</a>';	//membuat Link untuk kembali ke halaman tambah
-	} else {
-		echo 'Gagal menambahkan data! ';		//Pesan jika proses tambah gagal
-		echo '<a href="index.php">Kembali</a>';	//membuat Link untuk kembali ke halaman tambah
-		echo "Error: ".$sql.". ".mysqli_error($koneksi);
-	}
-
-}else{	//jika tidak terdeteksi tombol tambah di klik
-
-	//redirect atau dikembalikan ke halaman tambah
-	echo '<script>window.history.back()</script>';
-
+if (mysqli_query($koneksi, $sql)) {
+	echo 'Data berhasil ditambahkan! ';	
+} else {
+	echo 'Gagal menambahkan data! ';
+	echo "Error: ".$sql.". ".mysqli_error($koneksi);
 }
+
 
 ?>
 
