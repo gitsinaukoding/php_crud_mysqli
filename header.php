addfd548bd924565cbde2e752e26df2646685fe7
gitsinaukoding <footprintstshirt@gmail.com>
2021-08-09 11:36:11 +0700

    membuat html dan hapus php

--- header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Simple CRUD</title>
-</head>
-<body>
-	<h2>Simple CRUD</h2>
-
-    <p><a href="index.php">Beranda</a> / <a href="create.php">Tambah Data</a></p>
